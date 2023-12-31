b98186917f0093b1e1d22b88a606f93440a6efcf
enumura1 <akirayuto0718@gmail.com>
2023-12-31 13:14:35 +0900

    add:update pc transparent animation scirpt

---
+++ src/animation/UpdatePCTransparency.tsx
@@ -0,0 +1,28 @@
+/* eslint @typescript-eslint/no-explicit-any: 0 */
+
+// ライブラリのインポート
+import * as THREE from 'three';
+
+// インターフェース
+interface pcTransparencyProps {
+    room: THREE.Object3D;
+    lerp: any;
+    scaleParcent: any;
+}
+
+export default function updatePCTransparent(props:pcTransparencyProps) {
+
+    const {
+        room, 
+        lerp, 
+        scaleParcent
+    } = props;
+
+    const pcTransparency2 = lerp(1, 0, scaleParcent(54, 65));
+    room.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+            child.material.transparent = true;
+            child.material.opacity = pcTransparency2;
+        }
+    });
+}
