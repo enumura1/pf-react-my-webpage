d751300210a37e1f9f04f9157835e41e69ba787f
enumura1 <akirayuto0718@gmail.com>
2024-01-08 00:40:14 +0900

    add:icon ui and text shadow on h3tag

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -14,24 +14,27 @@ export default function Info(props: InfoProps) {
         <div className="info-ctr-elem-left">
           <h1 className="sectionTitle">■ Info</h1>
           <p>フロントメインのトピックを不定期で更新してます。</p>
+          <a href="https://qiita.com/enumura1">
           <h3>◆ Qiita</h3>
+          </a>
+          <p className="text-overflow">
+            <a href="https://qiita.com/enumura1/items/0aa345637529f582e1e6">
+            📗【Webサイト制作】駆け出しエンジニアが1か月で作成したポートフォリオサイトの振り返り
+            </a>
+          </p>
           <p className="text-overflow">
             <a href="https://qiita.com/enumura1/items/8449c2c0b00c8eeac6c6">
-              ・【React】react-three-fiberで自作の3Dモデルを手軽にWebサイトに表示する
+            📗【React】react-three-fiberで自作の3Dモデルを手軽にWebサイトに表示する
             </a>
           </p>
           <p className="text-overflow">
             <a href="https://qiita.com/enumura1/items/c1dee84b87fc0f0692ad">
-              ・【React×Blender】react-three-fiberで表示した3Dモデルに発光感（ブルーム）を付ける
-            </a>
-          </p>
-          <p className="text-overflow">
-            <a href="https://qiita.com/enumura1/items/062ec1f222d8fb6badce">
-              ・【JavaScript】Webページ内にタブ切り替えイベントを実装してみた
+            📗【React×Blender】react-three-fiberで表示した3Dモデルに発光感（ブルーム）を付ける
             </a>
           </p>
           <h3>◆ Blender</h3>
-          <p>Blenderで作成した3Dモデルを投稿しています。</p>
+          <p>Blenderで画像のような3Dモデル作成しています。</p>
+          <p>今年の目標はアバター作成です。</p>
         </div>
         <div className="info-ctr-elem-right">
           <div className="info-img-topCtr">
