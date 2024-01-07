1d19b685ec5a34b13457d7072ac39788a396a6fb
enumura1 <akirayuto0718@gmail.com>
2024-01-08 00:45:45 +0900

    fix:check topic icon

--- src/__test__/Makes.test.tsx
+++ src/__test__/Makes.test.tsx
@@ -5,11 +5,11 @@ import Makes from '../components/Makes';
 
 test('bottom container check words', () => {
   render(<Makes />);
-  const checkTextElement = screen.getByText('○スマホフレームApp（モバイルApp）');
+  const checkTextElement = screen.getByText('📱スマホフレームApp（モバイルApp）');
   const checkTextElement2 = screen.getByText('・記事：CSS（box-shadow）生成ツールの制作を振り返って');
-  const checkTextElement3 = screen.getByText('○sticker-memopad-tool（chrome拡張）');
+  const checkTextElement3 = screen.getByText('💻sticker-memopad-tool（chrome拡張）');
   const checkTextElement4 = screen.getByText('・記事：chrome拡張機能 をリリースしたので開発過程を振り返ってみた');
-  const checkTextElement5 = screen.getByText('○box shadow生成ツール（WebApp）');
+  const checkTextElement5 = screen.getByText('💻box shadow生成ツール（WebApp）');
   const checkTextElement6 = screen.getByText(
     '・記事：エンジニア１年目の初心者が約１か月でリリースしたモバイルアプリの制作過程',
   );
